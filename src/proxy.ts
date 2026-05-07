d8747ac165b2cbc39392c9f08bc83acccda4d4ca
Beka Chogovadze <chogovadzebeka@gmail.com>
2026-05-07 20:52:48 +0400

    fix performance on mobile

--- src/proxy.ts
+++ src/proxy.ts
@@ -33,7 +33,7 @@ export function proxy(request: NextRequest) {
   const url = request.nextUrl.clone();
   url.pathname = `/${defaultLocale}${pathname === "/" ? "" : pathname}`;
 
-  return NextResponse.rewrite(url);
+  return NextResponse.redirect(url);
 }
 
 export const config = {
